102263e8872ef38f53e7666f31a9301bfa28cb84
akshaydig <you@example.com>
2021-12-13 13:49:22 +0530

    Added: Functionality to find how random numbers we generate to get distinct coupons numbers

---
+++ src/com/bl/logicalprograms/CoupenNumbers.java
@@ -0,0 +1,33 @@
+package com.bl.logicalprograms;
+
+import java.util.Scanner;
+
+public class CoupenNumbers {
+	public static int getRandom(int n) {
+		return (int) (Math.random() * n);
+	}
+
+	public static int collect(int n) {
+		boolean[] isCollected = new boolean[n];
+		int count = 0;
+		int distinct = 0;
+		while (distinct < n) {
+			int value = getRandom(n);
+			System.out.println("The Random number is :" + value);
+			count++;
+			if (!isCollected[value]) {
+				distinct++;
+				isCollected[value] = true;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter N value");
+		int N = sc.nextInt();
+		System.out.println("The Number of Times random perforformed for getting " + N + " Distinct Random Numbers is :"
+				+ collect(N));
+	}
+}
